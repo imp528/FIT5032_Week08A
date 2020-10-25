016c299b22d8253341e744b0ceb7a7095ff0ef00
imp528 <ywuu0164@student.monash.edu>
2020-10-25 21:39:01 +0800

    Add project files.

---
+++ FIT5032_Week08A/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using FIT5032_Week08A.Models;
+using FIT5032_Week08A.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FIT5032_Week08A.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Please comment out these codes once you have registered your API key.
+            //EmailSender es = new EmailSender();
+            //es.RegisterAPIKey();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model, HttpPostedFileBase postedFileBase)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+                    //HttpPostedFileBase postedFileBase= model.Upload;
+
+                    EmailSender es = new EmailSender();
+                    es.Send(toEmail, subject, contents, postedFileBase);
+
+                    ViewBag.Result = "Email has been send.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+/*
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Image image, HttpPostedFileBase
+postedFile)
+        {
+            ModelState.Clear();
+            var myUniqueFileName = string.Format(@"{0}", Guid.NewGuid());
+            image.Path = myUniqueFileName;
+            TryValidateModel(image);
+            if (ModelState.IsValid)
+            {
+                string serverPath = Server.MapPath("~/Uploads/");
+                string fileExtension = Path.GetExtension(postedFile.FileName);
+                string filePath = image.Path + fileExtension;
+                image.Path = filePath;
+                postedFile.SaveAs(serverPath + image.Path);
+                //db.Images.Add(image);
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(image);
+        }*/
+
+    }
+}
---
+++ FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents, HttpPostedFileBase postedFileBase)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("ywuu0164@student.monash.edu");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            if (postedFileBase != null && postedFileBase.ContentLength > 0)
+            {
+                string theFileName = Path.GetFileName(postedFileBase.FileName);
+                byte[] fileBytes = new byte[postedFileBase.ContentLength];
+                using (BinaryReader theReader = new BinaryReader(postedFileBase.InputStream))
+                {
+                    fileBytes = theReader.ReadBytes(postedFileBase.ContentLength);
+                }
+                string dataAsString = Convert.ToBase64String(fileBytes);
+                msg.AddAttachment(theFileName, dataAsString);
+            }
+
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
